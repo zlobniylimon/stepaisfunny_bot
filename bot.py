b9c3ca221c16265cd5fbae25b7754bce74e78f66
Victor Smirnov <zlobniy.limon@gmail.com>
2021-05-07 01:15:31 +0300

    change polling to webhook

--- bot.py
+++ bot.py
@@ -6,6 +6,14 @@ import logging
 API_TOKEN = os.getenv('API_TOKEN')
 USER_ID = os.getenv('USER_ID')
 
+IP_HOST = os.getenv('HOST')
+WEBHOOK_HOST = f'https://{IP_HOST}'
+WEBHOOK_PATH = f'/webhook/{API_TOKEN}'
+WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
+
+WEBAPP_HOST = '0.0.0.0'
+WEBAPP_PORT = int(os.getenv('PORT'))
+
 logging.basicConfig(level=logging.INFO)
 
 bot = Bot(token=API_TOKEN)
@@ -30,5 +38,22 @@ async def sarcastic_reply(message):
 async def get_status(message):
     await message.reply('Я НЕ СПЛЮ')
 
+async def on_startup(dp):
+    await bot.set_webhook(WEBHOOK_URL)
+
+async def on_shutdown(dp):
+    logging.warning('Shutting down...')
+    await bot.delete_webhook()
+    logging.warning('Bye!')
+    
 if __name__=="__main__":
-    executor.start_polling(dp)
+    executor.start_webhook(
+            dispatcher=dp,
+            webhook_path=WEBHOOK_PATH,
+            on_startup=on_startup,
+            on_shutdown=on_shutdown,
+            skip_updates=True,
+            host=WEBAPP_HOST,
+            port=WEBAPP_PORT,
+            )
+

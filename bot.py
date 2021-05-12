e59881ee395e04d290f52ea7d0d2fba7acec36f9
Victor Smirnov <zlobniy.limon@gmail.com>
2021-05-12 16:20:34 +0300

    add answer to photo type

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
-from aiogram import Bot, Dispatcher, executor
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.dispatcher import filters
 import os
 import random
 import logging
@@ -7,7 +8,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 API_TOKEN = os.getenv('API_TOKEN')
-USER_ID = os.getenv('USER_ID')
+USER_ID = int(os.getenv('USER_ID'))
 
 logging.basicConfig(level=logging.INFO)
 
@@ -24,10 +25,16 @@ answer_var = (
         'КАКОЙ СМЕШНОЙ МАЛЬЧИК'
         )
 
-@dp.message_handler(lambda message: message.text.lower().startswith('внимание, анекдот'))
+@dp.message_handler(user_id = USER_ID)
+@dp.message_handler(content_types='photo')
+@dp.message_handler(lambda msg: msg.caption.lower().startwith('внимание, анекдот'))
 async def sarcastic_reply(message):
-    if message.from_user.id == int(USER_ID):
-        await message.reply(random.choice(answer_var))
+    await message.reply(random.choice(answer_var))
+
+@dp.message_handler(user_id = USER_ID)
+@dp.message_handler(filters.Text(contain=['внимание, анекдот'], ignore_case=True))
+async def sarcastic_reply(message):
+    await message.reply(random.choice(answer_var))
 
 @dp.message_handler(commands=['status'])
 async def get_status(message):
